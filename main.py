04ff97916925c6744c6727ffccd40070a41b0ddc
Joe Marriage <45903919+physi3@users.noreply.github.com>
2019-07-04 23:28:24 +0100

    Added Menu
    Almost tripled file size!

--- main.py
+++ main.py
@@ -49,7 +49,79 @@ def encode(lines):
       for x in i:
         temp=temp+str(Seg(len(x),x[0]))
       elist=elist+temp+"\n"
-      
+
     return elist
 
-print(decode(encode(read("LogoArt.txt")).split("\n")))
+#print(decode(encode(read("LogoArt.txt")).split("\n")))
+while True:
+  while True:
+    try:
+      inp = int(input("\nEnter RLE [0]\nDisplay ASCII art [1]\nConvert to ASCII art [2]\nConvert to RLE [3]\nQuit [4]\n>>> "))
+
+      if inp > 4:
+        print("That is not an option, enter a new number")
+      else:
+        break
+    except:
+      print("That is not an option, enter a new number")     
+  if inp == 0: #Enter RLE
+    while True:
+      try:
+        a = int(input("\nHow many lines of RLE would you like to enter? (Must be more than 2)\n>>> "))
+
+        if a <= 2:
+          print("\nThat is not an option, enter a new number\n")
+          continue
+        else:
+          break
+      except:
+        print("\nThat is not an option, enter a new number\n") 
+    RLE = []    
+    while True:
+      try:
+        for i in range(a):
+          RLE.append(input("\nEnter your line of RLE\n>>> "))
+        input("Here is your art: \n\n"+decode(RLE)+"\nPress enter to return to the main menu.")
+        break
+      except:
+        print("\nThat is not valid, please try again\n")
+  if inp == 1: #Display ASCII art
+    while True:
+      try:
+        inp = input("\nEnter ASCII art text file\n>>> ")
+        openFile = open(inp,"r")
+        print("\nHere's your ASCII art:\n\n"+openFile.read())
+        openFile.close()
+        input("Press enter to continue")
+        break
+      except:
+       print("That is not an option, enter a new file") 
+  if inp == 2: #Convert to ASCII art
+    while True:
+      try:
+        inp = input("\nEnter RLE text file\n>>> ")
+        print("\nHere's your ASCII art:\n\n"+decode(read(inp)))
+        input("Press enter to continue")
+        break
+      except:
+       print("That is not an option, enter a new file") 
+  if inp == 3: #Convert to RLE
+    while True:
+      try:
+        inp = input("\nEnter ASCII art text file\n>>> ")
+        openFile = open("NewRLE.txt","w+")
+        openFile.write(encode(read(inp)))
+        openFile.close()
+
+        openFile = open(inp,"r")
+        leng=len(openFile.read())
+        openFile.close()
+
+        print("\nYour RLE was stored in NewRLE.txt and "+str(leng-len(encode(read(inp))))+" characters were dropped in compression.\n")
+        input("Press enter to continue\n")
+        break
+      except:
+       print("That is not an option, enter a new file")
+  if inp == 4: #Quit
+    print("Okay, bye then.")  
+    break
